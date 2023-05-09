0b63c61811a10fb6c7f5a184d4035c2720ad3236
Sashi Dahal <shashi.eu@gmail.com>
2023-05-09 16:53:51 +0200

    updated code

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-phpinfo();
+$start_time = microtime(TRUE);
+echo "<div align=center><h4>" . gethostname() .  "</h4></div>";
+echo "<hr>";
+phpinfo(INFO_VARIABLES);
+
+$end_time = microtime(TRUE);
+$time_taken =($end_time - $start_time)*1000;
+$time_taken = round($time_taken,5);
+echo '<div align="center"><hr>Page generated in '.$time_taken.' seconds.</div>';
+?>
+
